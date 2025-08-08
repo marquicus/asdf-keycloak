3d8e61235f66ffed90b4d2336e780c6800266ff5
Marco A Rojas <marquicus@gmail.com>
2025-08-08 09:05:20 -0600

    [ENH] Working plugin

---
+++ bin/utils.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+set -x
+download_path() {
+  local version=$1
+  local tmp_download_dir=$2
+
+  echo "$tmp_download_dir/keycloak-${version}.tar.gz"
+}
+
+download_version() {
+  local install_type=$1
+  local version=$2
+  local download_path=$3
+  local download_url
+
+  download_url=$(download_url "$install_type" "$version")
+
+  [ -f $download_path ] && rm "$download_path"
+
+  echo "==> curl -Lo $download_path -C - $download_url"
+  curl -Lo "$download_path" -C - "$download_url" || exit 1
+}
+
+download_url() {
+  local install_type=$1
+  local version=$2
+
+  if [ "$install_type" = "version" ]; then
+    echo "https://github.com/keycloak/keycloak/releases/download/${version}/keycloak-${version}.tar.gz"
+  else
+    # otherwise it can be a branch name or commit sha
+    echo "https://github.com/keycloak/keycloak/archive/${version}.tar.gz"
+  fi
+
+  return
+}
+
+download_keycloak() {
+  local install_type=$1
+  local version=$2
+  local download_path=$3
+  local tmp_download_dir
+  local archive_path
+
+  if [ "$TMPDIR" = "" ]; then
+    tmp_download_dir=$(mktemp -d -t keycloak_build_XXXXXX)
+  else
+    tmp_download_dir=$TMPDIR
+  fi
+
+  archive_path=$(download_path "$version" "$tmp_download_dir")
+  download_version "$install_type" "$version" "$archive_path"
+
+  # running this in subshell
+  # we don't want to disturb current working dir
+  (
+    if ! type "tar" &>/dev/null; then
+      echo "ERROR: tar not found"
+      exit 1
+    fi
+
+    tar zxf "$archive_path" -C "$download_path" --strip-components=1 || exit 1
+  )
+}
